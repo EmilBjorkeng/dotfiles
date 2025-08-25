b415ba28bf9177e98831bea7a7dbb00364a6b64d
Emil Bjørkeng <emil.bjorkeng@gmail.com>
2025-08-25 21:29:52 +0200

    Update trailing spaces plugin

--- nvim/lua/trailing-spaces/init.lua
+++ nvim/lua/trailing-spaces/init.lua
@@ -1,11 +1,13 @@
-local utils = require("trailing-spaces.utils")
+local commands = require('trailing-spaces.commands')
+local autocmds = require('trailing-spaces.autocmds')
 
 local M = {}
 
 -- Rute calls made to this module to the functions
 -- in the other modules
-M.setup = utils.setup
-M.refresh = utils.refresh
-M.highlight_trailing_spaces = utils.highlight_trailing_spaces
+M.setup = function()
+    autocmds.setup()
+    commands.setup()
+end
 
 return M
