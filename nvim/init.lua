27e3263917e1d3ae7b0b105effe7425ffff77c27
Emil Bjørkeng <emil.bjorkeng@gmail.com>
2025-08-25 11:18:01 +0200

    Update nvim init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,4 +12,7 @@ vim.cmd.colorscheme('theme')
 vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true, nowait = true, expr = false }) -- Copy to clipboard
 vim.keymap.set('v', '<C-x>', '"+d', { noremap = true, silent = true, nowait = true, expr = false }) -- Cut to clipboard
 
+vim.keymap.set('v', '<TAB>', '>gv', { silent = true, desc = "Indent selection" })       -- Tab to indent selected text
+vim.keymap.set('v', '<S-TAB>', '<gv', { silent = true, desc = "Outdent selection" })    -- Shift+Tab to outdent selected text
+
 require('plugin-manager').setup()
