9940f32372d9d49c1e0d9c0de80439e5683b25cd
Emil Bjørkeng <emil.bjorkeng@gmail.com>
2025-08-25 21:10:47 +0200

    Update filemenu plugin

--- nvim/lua/filemenu/menu.lua
+++ nvim/lua/filemenu/menu.lua
@@ -1,7 +1,8 @@
-local filemenu = require("filemenu.filemenu")
+local core = require("filemenu.core")
 
 local buf, win, start_win
-local ns = vim.api.nvim_create_namespace("menu_cursor")
+local cursor_ns_id = vim.api.nvim_create_namespace("menu_cursor")
+local ns_id = vim.api.nvim_create_namespace("filemenu")
 
 local M = {}
 
@@ -15,27 +16,30 @@ local mappings = {
 
 function M.set_mappings()
     for k,v in pairs(mappings) do
-        vim.api.nvim_buf_set_keymap(buf, 'n', k, ':lua require("filemenu").'..v..'<CR>', {
+        vim.api.nvim_buf_set_keymap(buf, 'n', k, ':lua require("filemenu.core").'..v..'<CR>', {
             nowait = true, noremap = true, silent = true
         })
     end
 end
 
 function M.redraw()
-    local entries = filemenu.build_menu_lines()
+    local entries = core.build_menu_lines()
     local lines = {}
     for _, entry in ipairs(entries) do
         table.insert(lines, entry.display)
     end
 
-	vim.api.nvim_buf_set_option(buf, 'modifiable', true)
+    vim.bo[buf].modifiable = true
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
 
-    local width = filemenu.display_name_width + 3
+    local width = core.display_name_width + 3
     for i, entry in ipairs(entries) do
         if entry.is_dir then
             -- Highlight directories
-            vim.api.nvim_buf_add_highlight(buf, -1, "FilemenuBlue", i - 1, 0, width)
+            vim.api.nvim_buf_set_extmark(buf, ns_id, i - 1, 0, {
+                end_col = width,
+                hl_group = "FilemenuBlue"
+            })
         end
 
         -- Don't include the date in the mode coloring
@@ -44,25 +48,38 @@ function M.redraw()
         -- Mode highlight
         local line = vim.api.nvim_buf_get_lines(buf, i - 1, i, false)[1]
         for col = width, #line - date_length do
-            if line:sub(col, col) == "-" then
-                -- Highlight all the dashes
-                vim.api.nvim_buf_add_highlight(buf, -1, "FilemenuGrey", i - 1, col - 1, col)
+            local ch = line:sub(col, col)
+            local hl_group = nil
 
+            -- Highlight all the dashes
+            if ch == "-" then
+                hl_group = "FilemenuGrey"
             -- Highlight the different parts of the mode
-            elseif line:sub(col, col) == "d" then
-                vim.api.nvim_buf_add_highlight(buf, -1, "FilemenuBlue", i - 1, col - 1, col)
-            elseif line:sub(col, col) == "r" then
-                vim.api.nvim_buf_add_highlight(buf, -1, "FilemenuPurple", i - 1, col - 1, col)
-            elseif line:sub(col, col) == "w" then
-                vim.api.nvim_buf_add_highlight(buf, -1, "FilemenuOrange", i - 1, col - 1, col)
-            elseif line:sub(col, col) == "x" then
-                vim.api.nvim_buf_add_highlight(buf, -1, "FilemenuGreen", i - 1, col - 1, col)
+            elseif ch == "d" then
+                hl_group = "FilemenuBlue"
+            elseif ch == "r" then
+                hl_group = "FilemenuPurple"
+            elseif ch == "w" then
+                hl_group = "FilemenuOrange"
+            elseif ch == "x" then
+                hl_group = "FilemenuGreen"
+            end
+
+            if hl_group then
+                vim.api.nvim_buf_set_extmark(buf, ns_id, i - 1, col - 1, {
+                    end_col = col,
+                    hl_group = hl_group
+                })
             end
         end
         -- Highlight date
-        vim.api.nvim_buf_add_highlight(buf, -1, "FilemenuBlue", i - 1, width + 11, -1)
+        vim.api.nvim_buf_set_extmark(buf, ns_id, i - 1, width + 11, {
+            end_line = i - 1,
+            end_col = #line,
+            hl_group = "FilemenuBlue"
+        })
     end
-	vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+    vim.bo[buf].modifiable = false
 end
 
 function M.create_win()
@@ -95,15 +112,15 @@ function M.create_win()
     win = vim.api.nvim_get_current_win()
 
     -- Prevents no save errors
-	vim.api.nvim_buf_set_option(0, 'buftype', 'nofile')
+    vim.bo[0].buftype = "nofile"
     -- Disables swap files for the menu
-	vim.api.nvim_buf_set_option(0, 'swapfile', false)
+    vim.bo[0].swapfile = false
     -- Destroy buffer if hidden
-	vim.api.nvim_buf_set_option(0, 'bufhidden', 'wipe')
+    vim.bo[0].bufhidden = "wipe"
 
     -- Local settings for the menu
-	vim.api.nvim_command('setlocal wrap')
-    vim.api.nvim_win_set_option(win, 'winhighlight', 'Normal:Normal,FloatBorder:Normal,FloatTitle:Normal')
+    vim.api.nvim_command('setlocal wrap')
+    vim.wo[win].winhl = "Normal:Normal,FloatBorder:Normal,FloatTitle:Normal"
 
     -- Set custom cursor in the menu and movement restriction
     vim.api.nvim_create_autocmd("CursorMoved", {
@@ -113,10 +130,10 @@ function M.create_win()
             local line = vim.api.nvim_win_get_cursor(0)[1] - 1
 
             -- Clear all extmarks in this namespace for the buffer
-            vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+            vim.api.nvim_buf_clear_namespace(buf, cursor_ns_id, 0, -1)
 
             -- Set virt text ">" with a highlight group, at col 0
-            vim.api.nvim_buf_set_extmark(buf, ns, line, 0, {
+            vim.api.nvim_buf_set_extmark(buf, cursor_ns_id, line, 0, {
                 virt_text = { { ">", "Normal" } },
                 virt_text_pos = "overlay",
             })
@@ -131,12 +148,12 @@ function M.open_menu()
     local dir = path:match("(.*/)")
     vim.fn.chdir(dir)
 
-   	if win and vim.api.nvim_win_is_valid(win) then
-		vim.api.nvim_set_current_win(win)
-	else
-		M.create_win()
-	end
-	M.redraw()
+       if win and vim.api.nvim_win_is_valid(win) then
+        vim.api.nvim_set_current_win(win)
+    else
+        M.create_win()
+    end
+    M.redraw()
 end
 
 function M.close_menu()
