c7616534e320e51a9d821f4eb646e97fe7723d80
Emil Bjørkeng <emil.bjorkeng@gmail.com>
2025-09-15 22:17:53 +0200

    Add plugins.lua

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,68 @@
+return {
+    {
+        name = 'Line Comment',
+        module = 'line-comment',
+        lazy = { event = 'BufReadPost' },
+        config = true,
+    },
+    {
+        name = 'Trailing Spaces',
+        module = 'trailing-spaces',
+        lazy = { event = { 'BufReadPost', 'BufNewFile' } },
+        config = true,
+    },
+    {
+        name = 'Filemenu',
+        module = 'filemenu',
+        lazy = { cmd = 'File' },
+        config = true,
+    },
+    {
+        name = 'LSP',
+        module = 'lsp',
+        lazy = { ft = {
+            'lua', 'c', 'cpp', 'objc', 'objcpp',
+            'python', 'rust',
+            'html', 'css', 'scss', 'less',
+            'javascript', 'typescript'
+        }},
+        config = true,
+    },
+    {
+        name = 'Hexcolor',
+        module = 'hexcolor',
+        lazy = { event = { 'BufReadPost', 'BufNewFile' } },
+        config = function(plugin)
+            require(plugin.module).setup({
+                autoshow = true,
+            })
+        end,
+    },
+    {
+        name = 'Git status',
+        module = 'git-status',
+        lazy = { event = { 'BufReadPost', 'BufNewFile' } },
+        config = function(plugin)
+            require(plugin.module).setup({
+                autoshow = true,
+            })
+        end,
+    },
+    {
+        name = 'Hidden Tabs',
+        module = 'hidden-tabs',
+        lazy = { event = { 'BufReadPost', 'BufNewFile' } },
+        config = function(plugin)
+            require(plugin.module).setup({
+                autoshow = true,
+            })
+        end,
+    },
+    {
+        name = 'Greeter',
+        module = 'greeter',
+        lazy = { event = 'VimEnter' },
+        config = true,
+        dependencies = { 'filemenu' },
+    },
+}
