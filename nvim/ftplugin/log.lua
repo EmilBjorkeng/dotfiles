8732393e410e242c07b193b9234aaf8e8897fa69
Emil Bjørkeng <emil.bjorkeng@gmail.com>
2026-03-02 18:19:12 +0100

    Add more colors to the log color file

--- nvim/ftplugin/log.lua
+++ nvim/ftplugin/log.lua
@@ -1,14 +1,20 @@
-vim.api.nvim_set_hl(0, 'LogInfo',     { fg = '#4FC3F7' })
+vim.api.nvim_set_hl(0, 'LogBracket', { fg = '#61808F' })
+vim.fn.matchadd('LogBracket', '\\[\\zs.\\{-}\\ze\\]')
+
+vim.api.nvim_set_hl(0, 'LogInfo', { fg = '#4FC3F7' })
 vim.fn.matchadd('LogInfo', [[\[\zsINFO\ze]])
 
-vim.api.nvim_set_hl(0, 'LogWarning',  { fg = '#FFB300' })
+vim.api.nvim_set_hl(0, 'LogWarning', { fg = '#FFB300' })
 vim.fn.matchadd('LogWarning', [[\[\zsWARNING\ze]])
 
-vim.api.nvim_set_hl(0, 'LogError',    { fg = '#F44336' })
+vim.api.nvim_set_hl(0, 'LogError', { fg = '#F44336' })
 vim.fn.matchadd('LogError', [[\[\zsERROR\ze]])
 
 vim.api.nvim_set_hl(0, 'LogCritical', { fg = '#D32F2F', bold = true })
 vim.fn.matchadd('LogCritical', [[\[\zsCRITICAL\ze]])
 
-vim.api.nvim_set_hl(0, 'LogDebug',    { fg = '#66BB6A' })
+vim.api.nvim_set_hl(0, 'LogDebug', { fg = '#66BB6A' })
 vim.fn.matchadd('LogDebug', [[\[\zsDEBUG\ze]])
+
+vim.api.nvim_set_hl(0, 'LogIP', { fg = '#9C5AA0' })
+vim.fn.matchadd('LogIP', [[\v\d{1,3}(\.\d{1,3}){3}(:\d+)?]])
