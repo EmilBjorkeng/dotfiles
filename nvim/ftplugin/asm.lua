6706cb55d04ff0d5543bcd58b6b8db9019be8dbb
Emil Bjørkeng <emil.bjorkeng@gmail.com>
2026-03-02 13:09:49 +0100

    Update the format of the assembly color file

--- nvim/ftplugin/asm.lua
+++ nvim/ftplugin/asm.lua
@@ -8,112 +8,80 @@ set.expandtab=true
 --  Syntax highlighting  --
 ---------------------------
 
--- Base Colour
-vim.cmd([[highlight baseColour ctermfg=Cyan]])
-vim.fn.matchadd('baseColour', '.')
+-- Base Color
+vim.api.nvim_set_hl(0, 'baseColor', {ctermfg='Cyan'})
+vim.fn.matchadd('baseColor', '.')
 
 -- Comments
-vim.cmd([[highlight commentColour ctermfg=Grey]])
-vim.fn.matchadd('commentColour', ';\\+.*')
+vim.api.nvim_set_hl(0, 'commentColor', {ctermfg='Grey'})
+vim.fn.matchadd('commentColor', ';.+')
 
 -- Quotes
-vim.cmd([[highlight quoteColour ctermfg=LightMagenta]])
-vim.fn.matchadd('quoteColour', '".\\{-}"')
-vim.fn.matchadd('quoteColour', "'.\\{-}'")
+vim.api.nvim_set_hl(0, 'quoteColor', {ctermfg='LightMagenta'})
+vim.fn.matchadd('quoteColor', '".-?"')
+vim.fn.matchadd('quoteColor', "'.-?'")
 
 -- Commands
-vim.cmd([[highlight commandColour ctermfg=blue]])
-vim.fn.matchadd('commandColour', '^\\s\\+\\w\\+\\|^\\w\\+')
+vim.api.nvim_set_hl(0, 'commandColor', {ctermfg='Blue'})
+vim.fn.matchadd('commandColor', '^%s*%w+')
 
 -- Jump
-vim.cmd([[highlight jumpColour ctermfg=Yellow]])
-vim.fn.matchadd('jumpColour', '^\\<jmp\\>\\|^\\s\\+\\<jmp\\>')
-vim.fn.matchadd('jumpColour', '^\\<je\\>\\|^\\s\\+\\<je\\>')
-vim.fn.matchadd('jumpColour', '^\\<jne\\>\\|^\\s\\+\\<jne\\>')
-vim.fn.matchadd('jumpColour', '^\\<jg\\>\\|^\\s\\+\\<jg\\>')
-vim.fn.matchadd('jumpColour', '^\\<jge\\>\\|^\\s\\+\\<jge\\>')
-vim.fn.matchadd('jumpColour', '^\\<jl\\>\\|^\\s\\+\\<jl\\>')
-vim.fn.matchadd('jumpColour', '^\\<jle\\>\\|^\\s\\+\\<jle\\>')
-vim.fn.matchadd('jumpColour', '^\\<ja\\>\\|^\\s\\+\\<ja\\>')
-vim.fn.matchadd('jumpColour', '^\\<jae\\>\\|^\\s\\+\\<jae\\>')
-vim.fn.matchadd('jumpColour', '^\\<jb\\>\\|^\\s\\+\\<jb\\>')
-vim.fn.matchadd('jumpColour', '^\\<jbe\\>\\|^\\s\\+\\<jbe\\>')
-vim.fn.matchadd('jumpColour', '^\\<jc\\>\\|^\\s\\+\\<jc\\>')
-vim.fn.matchadd('jumpColour', '^\\<jnc\\>\\|^\\s\\+\\<jnc\\>')
-vim.fn.matchadd('jumpColour', '^\\<jz\\>\\|^\\s\\+\\<jz\\>')
-vim.fn.matchadd('jumpColour', '^\\<jnz\\>\\|^\\s\\+\\<jnz\\>')
-vim.fn.matchadd('jumpColour', '^\\<js\\>\\|^\\s\\+\\<js\\>')
-vim.fn.matchadd('jumpColour', '^\\<jns\\>\\|^\\s\\+\\<jns\\>')
-vim.fn.matchadd('jumpColour', '^\\<jp\\>\\|^\\s\\+\\<jp\\>')
-vim.fn.matchadd('jumpColour', '^\\<jnp\\>\\|^\\s\\+\\<jnp\\>')
-vim.fn.matchadd('jumpColour', '^\\<jo\\>\\|^\\s\\+\\<jo\\>')
-vim.fn.matchadd('jumpColour', '^\\<jno\\>\\|^\\s\\+\\<jno\\>')
+vim.api.nvim_set_hl(0, 'jumpColor', {ctermfg='Yellow'})
+local jumps = {
+    'jmp','je','jne','jg','jge','jl','jle','ja','jae','jb','jbe',
+    'jc','jnc','jz','jnz','js','jns','jp','jnp','jo','jno'
+}
+for _, j in ipairs(jumps) do
+    vim.fn.matchadd('jumpColor', '^%s*<'..j..'>')
+end
 
 -- Label
-vim.cmd([[highlight labelColour ctermfg=White]])
-vim.fn.matchadd('labelColour', '^\\w\\+:')
+vim.api.nvim_set_hl(0, 'labelColor', {ctermfg='White'})
+vim.fn.matchadd('labelColor', '^%w+:')
 
 -- Global (_start)
-vim.cmd([[highlight globalColour ctermfg=Yellow]])
-vim.fn.matchadd('globalColour', '^global')
+vim.api.nvim_set_hl(0, 'globalColor', {ctermfg='Yellow'})
+vim.fn.matchadd('globalColor', '^global')
 
 -- Secrion (.text)
-vim.cmd([[highlight sectiontypeColour ctermfg=Yellow]])
-vim.fn.matchadd('sectiontypeColour', '^section .*')
+vim.api.nvim_set_hl(0, 'sectiontypeColor', {ctermfg='Yellow'})
+vim.fn.matchadd('sectiontypeColor', '^section .*')
 
 -- Push/Pop
-vim.cmd([[highlight pushColour ctermfg=DarkYellow]])
-vim.fn.matchadd('pushColour', '^\\<push\\>\\|^\\s\\+\\<push\\>')
-vim.fn.matchadd('pushColour', '^\\<pop\\>\\|^\\s\\+\\<pop\\>')
+vim.api.nvim_set_hl(0, 'pushColor', {ctermfg='DarkYellow'})
+vim.fn.matchadd('pushColor', '^%s*<(push|pop)>')
 
 -- Numbers
-vim.cmd([[highlight numberColour ctermfg=Magenta]])
-vim.fn.matchadd('numberColour', '0x\\S*\\|\\d')
+vim.api.nvim_set_hl(0, 'numberColor', {ctermfg='Magenta'})
+vim.fn.matchadd('numberColor', '0x%S*|%d')
 
 -- 8 bit register
-vim.cmd([[highlight registerColour8 ctermfg=Red]])
-vim.fn.matchadd('registerColour8', '\\<al\\>')
-vim.fn.matchadd('registerColour8', '\\<bl\\>')
-vim.fn.matchadd('registerColour8', '\\<cl\\>')
-vim.fn.matchadd('registerColour8', '\\<dl\\>')
+vim.api.nvim_set_hl(0, 'registerColor8', {ctermfg='Red'})
+local r8 = {'al','bl','cl','dl'}
+for _, reg in ipairs(r8) do
+    vim.fn.matchadd('registerColor8', '<'..reg..'>')
+end
 
 -- 16 bit register
-vim.cmd([[highlight registerColour16 ctermfg=Red]])
-vim.fn.matchadd('registerColour16', '\\<ax\\>')
-vim.fn.matchadd('registerColour16', '\\<bx\\>')
-vim.fn.matchadd('registerColour16', '\\<cx\\>')
-vim.fn.matchadd('registerColour16', '\\<dx\\>')
-vim.fn.matchadd('registerColour16', '\\<si\\>')
-vim.fn.matchadd('registerColour16', '\\<di\\>')
-vim.fn.matchadd('registerColour16', '\\<bp\\>')
-vim.fn.matchadd('registerColour16', '\\<sp\\>')
+vim.api.nvim_set_hl(0, 'registerColor16', {ctermfg='Red'})
+local r16 = {'ax','bx','cx','dx','si','di','bp','sp'}
+for _, reg in ipairs(r16) do
+    vim.fn.matchadd('registerColor16', '<'..reg..'>')
+end
 
 -- 32 bit register
-vim.cmd([[highlight registerColour32 ctermfg=Red]])
-vim.fn.matchadd('registerColour32', '\\<eax\\>')
-vim.fn.matchadd('registerColour32', '\\<ebx\\>')
-vim.fn.matchadd('registerColour32', '\\<ecx\\>')
-vim.fn.matchadd('registerColour32', '\\<edx\\>')
-vim.fn.matchadd('registerColour32', '\\<esi\\>')
-vim.fn.matchadd('registerColour32', '\\<edi\\>')
-vim.fn.matchadd('registerColour32', '\\<ebp\\>')
-vim.fn.matchadd('registerColour32', '\\<esp\\>')
+vim.api.nvim_set_hl(0, 'registerColor32', {ctermfg='Red'})
+local r32 = {'eax','ebx','ecx','edx','esi','edi','ebp','esp'}
+for _, reg in ipairs(r32) do
+    vim.fn.matchadd('registerColor32', '<'..reg..'>')
+end
 
 -- 64 bit registers
-vim.cmd([[highlight registerColour64 ctermfg=Red]])
-vim.fn.matchadd('registerColour64', '\\<rax\\>')
-vim.fn.matchadd('registerColour64', '\\<rbx\\>')
-vim.fn.matchadd('registerColour64', '\\<rcx\\>')
-vim.fn.matchadd('registerColour64', '\\<rdx\\>')
-vim.fn.matchadd('registerColour64', '\\<rsi\\>')
-vim.fn.matchadd('registerColour64', '\\<rdi\\>')
-vim.fn.matchadd('registerColour64', '\\<rbp\\>')
-vim.fn.matchadd('registerColour64', '\\<rsp\\>')
-vim.fn.matchadd('registerColour64', '\\<r8\\>')
-vim.fn.matchadd('registerColour64', '\\<r9\\>')
-vim.fn.matchadd('registerColour64', '\\<r10\\>')
-vim.fn.matchadd('registerColour64', '\\<r11\\>')
-vim.fn.matchadd('registerColour64', '\\<r12\\>')
-vim.fn.matchadd('registerColour64', '\\<r13\\>')
-vim.fn.matchadd('registerColour64', '\\<r14\\>')
-vim.fn.matchadd('registerColour64', '\\<r15\\>')
+vim.api.nvim_set_hl(0, 'registerColor64', {ctermfg='Red'})
+local r64 = {
+    'rax','rbx','rcx','rdx','rsi','rdi','rbp','rsp',
+    'r8','r9','r10','r11','r12','r13','r14','r15'
+}
+for _, reg in ipairs(r64) do
+    vim.fn.matchadd('registerColor64', '<'..reg..'>')
+end
